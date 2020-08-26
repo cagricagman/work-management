58dea181dc43a7990b1247bc58f902ab7da477bb
cagricagman <cagri.cagman@gmail.com>
2020-08-26 10:28:58 +0300

    Users controller'ı oluşturuldu. Users view ve userop view klasör yapısı oluşturuldu. editlenerek gerekli kodlamalar yapılacak.

---
+++ application/controllers/Users.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Users extends CI_Controller {
+
+	public $viewFolder = "";
+
+	public function __construct()
+    {
+        parent::__construct();
+        $this->viewFolder = "users_v";
+
+//        if (!get_active_user()){
+//            redirect(base_url("login"));
+//        }
+    }
+
+    public function index()
+	{
+        $viewData = new stdClass();
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "list";
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index",$viewData);
+	}
+}
